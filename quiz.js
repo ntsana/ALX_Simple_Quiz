bfb35ce83678a82422740cc8d55f7711a8500b76
ntsana <ntsanapheko@gmail.com>
2025-01-19 22:46:17 +0200

    added

---
+++ quiz.js
@@ -0,0 +1,25 @@
+function checkAnswer() {
+   
+    const correctAnswer = "4";
+
+ 
+    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+
+    const feedback = document.getElementById("feedback");
+
+    if (!userAnswer) {
+        feedback.textContent = "Please select an answer!";
+    } else if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+
+
+
+
+
